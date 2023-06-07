4142629413dcc5e0e9863b7f3d43c1d17c138dab
Ivan Skalyga <vanya729@ukr.net>
2023-06-07 16:05:42 +0300

    homework 11 Plurals

---
+++ src/main/java/lessons10/homework1Plurals/CoffeeOrderBoard.java
@@ -0,0 +1,72 @@
+package lessons10.homework1Plurals;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CoffeeOrderBoard {
+    Map<Integer, Order> listOrder;
+    int lastOrder;
+
+    public CoffeeOrderBoard() {
+        listOrder = new HashMap<>();
+        lastOrder=0;
+    }
+
+    public void add(String name){
+        lastOrder++;
+        Order newOrder = new Order(lastOrder, name);
+        listOrder.put(lastOrder,newOrder);
+    }
+
+
+    public void deliver(){
+        if (!listOrder.isEmpty()){
+            int orderNumKey = listOrder.keySet().iterator().next();
+            listOrder.remove(orderNumKey);
+            System.out.println( "Delivered order number: "+ orderNumKey);
+        }else {
+            System.out.println("No orders in the queue.");
+        }
+    }
+
+    public void deliverWithNum(int orderNum){
+        if (listOrder.containsKey(orderNum)){
+            listOrder.remove(orderNum);
+            System.out.println("Delivered order number: " + orderNum);
+        }else {
+            System.out.println("Order number " + orderNum + " does not exist.");
+        }
+    }
+
+    public void draw() {
+        for (Order order :  listOrder.values()) {
+            System.out.println("===============================");
+            System.out.println(order.getNumber()+ " | " + order.getName());
+        }
+        System.out.println("===============================");
+    }
+
+    public Map<Integer, Order> getOrders() {
+        return listOrder;
+    }
+}
+
+/*
+6. Реалізувати метод draw у класі CoffeeOrderBoard.
+ Цей метод виводить у консоль інформацію про поточний стан черги у порядку
+ найближчого до видачі замовлення.
+
+Наприклад:
+
+=============
+
+Num | Name
+
+4 | Alen
+
+27 | Yoda
+
+33 | Obi-van
+
+34 | John Snow
+ */
---
+++ src/main/java/lessons10/homework1Plurals/Main.java
@@ -0,0 +1,37 @@
+package lessons10.homework1Plurals;
+
+
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+
+        CoffeeOrderBoard coffeeOrderBoard = new CoffeeOrderBoard();
+
+        coffeeOrderBoard.add("Vanya");
+        coffeeOrderBoard.add("Alex");
+        coffeeOrderBoard.add("Oleg");
+
+        coffeeOrderBoard.draw();
+
+        Map<Integer, Order> orders = coffeeOrderBoard.getOrders();
+        System.out.println("Current orders: " + orders);
+
+        coffeeOrderBoard.deliver();
+        System.out.println("After delivery:");
+        orders = coffeeOrderBoard.getOrders();
+        System.out.println("Current orders: " + orders);
+
+        int orderNum = 2;
+        coffeeOrderBoard.deliverWithNum(orderNum);
+        System.out.println("After delivering order number " + orderNum + ":");
+        orders = coffeeOrderBoard.getOrders();
+        System.out.println("Current orders: " + orders);
+
+        coffeeOrderBoard.draw();
+
+
+    }
+
+}
+
